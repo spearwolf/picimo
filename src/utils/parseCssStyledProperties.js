7f613a16607672f6b2fbbd29f81220442dd6d83a
Wolfger Schramm <wolfger@spearwolf.de>
2017-08-08 13:15:29 +0200

    improve parseCssStyledProperties()

--- src/utils/parseCssStyledProperties.js
+++ src/utils/parseCssStyledProperties.js
@@ -14,11 +14,14 @@ const reNumber = /^-?\d+(\.\d+)?$/
 //   undefined  => undefined
 //   true       => boolean: true
 //   false      => boolean: false
+//   {...}      => JSON.parse()
 //
 function parseValue (value) {
   const len = value.length
   if (len >= 2 && value.startsWith('"') && value.endsWith('"')) {
     return value.substr(1, value.length - 2)
+  } else if (len >= 2 && value.startsWith('{') && value.endsWith('}')) {
+    return JSON.parse(value)
   } else if (len >= 2 && value.startsWith('\'') && value.endsWith('\'')) {
     return value.substr(1, value.length - 2)
   } else if (len > 0 && reNumber.exec(value)) {
@@ -35,34 +38,136 @@ function parseValue (value) {
   return value
 }
 
-function parseProperty (map, property) {
-  if (property.includes(':')) {
-    let [ key, value ] = property.split(':', 2)
-    key = key.trim()
-    value = value.trim()
-    if (key && value) {
-      map[key] = parseValue(value)
+function indexOfNextNonWhitespace (str, curIdx) {
+  const len = str.length
+  let i = curIdx
+  if (i >= len) return len
+  do {
+    if (str[i] === ' ' || str[i] === '\t') {
+      ++i
+    } else {
+      return i
     }
-  }
+  } while (i < len)
+  return len
 }
 
-// module.exports = function (data) {
-export default function (data) {
+function indexOfNextSeperator (str, curIdx) {
+  const len = str.length
+  let i = curIdx
+  let isInside = null
+  do {
+    if (isInside === null) {
+      switch (str[i]) {
+        case ';': return i
+        case '\'':
+          isInside = '\''
+          ++i
+          break
+        case '"':
+          isInside = '"'
+          ++i
+          break
+        case '(':
+          isInside = ')'
+          ++i
+          break
+        case '{':
+          isInside = '}'
+          ++i
+          break
+        case '\\':
+          i += 2
+          break
+        default:
+          ++i
+      }
+    } else {
+      if (str[i] === isInside) {
+        isInside = null
+        ++i
+      } else if (str[i] === '\\') {
+        i += 2
+      } else {
+        ++i
+      }
+    }
+  } while (i < len)
+  return len
+}
+
+function splitIntoPropTokens (str) {
+  if (typeof str !== 'string' || !str) return
+
+  const propTokens = []
+  const len = str.length
+
+  let i = indexOfNextNonWhitespace(str, 0)
+  do {
+    const to = indexOfNextSeperator(str, i)
+    if (to > i) {
+      propTokens.push(str.slice(i, to))
+    }
+    i = indexOfNextNonWhitespace(str, to + 1)
+  } while (i < len)
+
+  return propTokens
+}
+
+function splitIntoProps (str) {
+  const tokens = splitIntoPropTokens(str)
+  if (!tokens) return
+  return tokens.map((tok) => {
+    const colon = tok.indexOf(':')
+    if (colon === -1) {
+      return { value: tok.trim() }
+    }
+    const key = tok.slice(0, colon).trim()
+    const value = tok.substr(colon + 1).trim()
+    return {
+      key,
+      value
+    }
+  }).filter((prop) => !(prop.key === '' && prop.value === ''))
+}
+
+function parseCssStyledProperties (data) {
   if (typeof data !== 'string') return data
 
+  // => json
   const str = data.trim()
   if (str.startsWith('{') && str.endsWith('}')) {
     return JSON.parse(str)
   }
 
-  if (str.includes(';') || str.includes(':')) {
-    const propsMaps = {}
-    const props = str.split(';')
-    for (let i = 0; i < props.length; ++i) {
-      parseProperty(propsMaps, props[i].trim())
-    }
-    return propsMaps
-  }
+  const props = splitIntoProps(str)
 
-  return parseValue(str)
+  // => undefined
+  if (!props || props.length === 0) return
+
+  if ('key' in props[0]) {
+    // => properties
+    const map = {}
+    props.forEach(({ key, value }) => {
+      map[key] = parseValue(value)
+    })
+    return map
+  } else {
+    // => array of values OR single value
+    const values = props.map(({ value }) => parseValue(value))
+    return values.length === 1 ? values[0] : values
+  }
+}
+
+// module.exports = parseCssStyledProperties
+// module.exports.splitIntoPropTokens = splitIntoPropTokens
+// module.exports.splitIntoProps = splitIntoProps
+// module.exports.indexOfNextNonWhitespace = indexOfNextNonWhitespace
+// module.exports.indexOfNextSeperator = indexOfNextSeperator
+export default parseCssStyledProperties
+export {
+  splitIntoPropTokens,
+  splitIntoProps,
+  indexOfNextNonWhitespace,
+  indexOfNextSeperator
 }
---
+++ test/test_utils_parseCssStyledProperties.js
@@ -0,0 +1,181 @@
+/* eslint-env mocha */
+import { expect } from 'chai'
+import parse, {
+  splitIntoPropTokens,
+  splitIntoProps,
+  indexOfNextNonWhitespace,
+  indexOfNextSeperator
+} from 'src/utils/parseCssStyledProperties.js'
+
+describe('parseCssStyledProperties()', () => {
+  describe('indexOfNextSeperator', () => {
+    it('empty string', () => {
+      expect(indexOfNextSeperator('', 0)).to.equal(0)
+    })
+    it('blank string', () => {
+      expect(indexOfNextSeperator('   ', 0)).to.equal(3)
+    })
+    it('a semicolon', () => {
+      expect(indexOfNextSeperator(';', 0)).to.equal(0)
+    })
+    it('mutiple chars followed by a semicolon', () => {
+      expect(indexOfNextSeperator('x z;', 0)).to.equal(3)
+    })
+    it('double quotes', () => {
+      expect(indexOfNextSeperator('x: "z;\\"";', 0)).to.equal(9)
+    })
+    it('single quotes', () => {
+      expect(indexOfNextSeperator('x: \'z; " \' ;', 0)).to.equal(11)
+    })
+    it('brackets', () => {
+      expect(indexOfNextSeperator('x: (z;\\");', 0)).to.equal(9)
+    })
+  })
+
+  describe('indexOfNextNonWhitespace', () => {
+    it('no whitespace', () => {
+      expect(indexOfNextNonWhitespace('x', 0)).to.equal(0)
+    })
+    it('one whitespace', () => {
+      expect(indexOfNextNonWhitespace(' x', 0)).to.equal(1)
+    })
+    it('multiple whitespaces', () => {
+      expect(indexOfNextNonWhitespace('   x', 0)).to.equal(3)
+    })
+    it('empty string', () => {
+      expect(indexOfNextNonWhitespace('', 0)).to.equal(0)
+    })
+    it('multiple whitespaces until end of string', () => {
+      expect(indexOfNextNonWhitespace('     ', 0)).to.equal(5)
+    })
+  })
+
+  describe('splitIntoProps', () => {
+    it('should work', () => {
+      expect(splitIntoProps(' a:b  ;  c: d')).to.eql([
+        { key: 'a', value: 'b' },
+        { key: 'c', value: 'd' }
+      ])
+    })
+    it('should respect double quotes', () => {
+      expect(splitIntoProps(' a: "basd ;"  ;  c: d')).to.eql([
+        { key: 'a', value: '"basd ;"' },
+        { key: 'c', value: 'd' }
+      ])
+    })
+    it('a single value', () => {
+      expect(splitIntoProps(' abc(";) ')).to.eql([
+        { value: 'abc(";)' }
+      ])
+    })
+    it('multiple values', () => {
+      expect(splitIntoProps(' abc(";) ; 123.0;')).to.eql([
+        { value: 'abc(";)' },
+        { value: '123.0' }
+      ])
+    })
+    it('empty string', () => {
+      expect(splitIntoProps('')).to.equal(undefined)
+    })
+    it('blank string', () => {
+      expect(splitIntoProps('   ')).to.eql([])
+    })
+    it('blank string with colons and semicolons', () => {
+      expect(splitIntoProps(' ;; ; ;:;;  : ;; ;')).to.eql([])
+    })
+  })
+
+  describe('splitIntoPropTokens', () => {
+    it('should work', () => {
+      expect(splitIntoPropTokens(' a:b  ;  c: d')).to.eql([
+        'a:b  ', 'c: d'
+      ])
+    })
+  })
+
+  describe('parse', () => {
+    it('should return an object with properties separated by ";"', () => {
+      expect(parse('a: 123; b: foo')).to.eql({
+        a: 123,
+        b: 'foo'
+      })
+    })
+
+    describe('should transform single values', () => {
+      it('single-quotes string', () => {
+        expect(parse('\'foo\'')).to.be.equal('foo')
+      })
+      it('double-quotes string', () => {
+        expect(parse(' "foo"')).to.be.equal('foo')
+      })
+      it('positive integer', () => {
+        expect(parse('123')).to.be.equal(123)
+      })
+      it('negative integer', () => {
+        expect(parse('-123')).to.be.equal(-123)
+      })
+      it('positive float', () => {
+        expect(parse('4.567')).to.be.equal(4.567)
+      })
+      it('negative float', () => {
+        expect(parse('-66.123')).to.be.equal(-66.123)
+      })
+      it('truthy boolean', () => {
+        expect(parse('true')).to.be.equal(true)
+      })
+      it('falsy boolean', () => {
+        expect(parse('false')).to.be.equal(false)
+      })
+      it('null', () => {
+        expect(parse('null')).to.be.equal(null)
+      })
+      it('undefined', () => {
+        expect(parse('undefined')).to.be.equal(undefined)
+      })
+    })
+
+    describe('should return unprocessed input if input is ..', () => {
+      it('undefined', () => {
+        expect(parse()).to.be.equal(undefined)
+      })
+      it('null', () => {
+        expect(parse(null)).to.be.equal(null)
+      })
+      it('a number', () => {
+        expect(parse(123)).to.be.equal(123)
+      })
+      it('a boolean', () => {
+        expect(parse(true)).to.be.equal(true)
+      })
+      it('an object', () => {
+        const o = {}
+        expect(parse(o)).to.be.equal(o)
+      })
+      it('a function', () => {
+        const f = () => true
+        expect(parse(f)).to.be.equal(f)
+      })
+    })
+
+    describe('should parse json like input', () => {
+      it('json as single value', () => {
+        expect(parse('{ "fo;o": 123, "bar": "pla;h", "plah": true }')).to.eql({
+          'fo;o': 123,
+          bar: 'pla;h',
+          plah: true
+        })
+      })
+      it('as value inside css like properties', () => {
+        expect(parse('abc: 456; def: { "fo;o": 123, "bar": "pla;h", "plah": true }; ghi: 789')).to.eql({
+          abc: 456,
+          def: {
+            'fo;o': 123,
+            bar: 'pla;h',
+            plah: true
+          },
+          ghi: 789
+        })
+      })
+    })
+  })
+})
