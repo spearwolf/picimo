da4b9bd9445d7c3845ee5241f9d99a8d4c9a6815
Wolfger Schramm <wolfger@spearwolf.de>
2017-08-08 10:01:16 +0200

    add simple string search to javascript-imports.sh

--- scripts/javascript-imports.js
+++ scripts/javascript-imports.js
@@ -6,14 +6,16 @@ const { execSync } = require('child_process')
 
 program
   .version('0.2.0')
+  .usage('[options] [<search>]')
   .option('-R, --raw', 'Show raw import statements')
-  .option('-0, --zero', 'Show the full list of all imports')
+  .option('-0, --zero', 'Show the full list of all imported source files')
   .option('-J, --json', 'Json output')
   .parse(process.argv)
 
 const RAW = program.raw && !program.json
 const ZERO = program.zero && !program.json
 const JSON_OUTPUT = program.json
+const SEARCH = !JSON_OUTPUT && !RAW && program.args[0]
 
 const filesList = execSync('./source-files.sh', { cwd: __dirname, encoding: 'utf-8' }).split('\n')
 
@@ -68,16 +70,31 @@ filesList.forEach((filename) => {
 
   // step-2-a) show full imports info as human readable text (for current source file)
 
-  if (!RAW && !JSON_OUTPUT) {
+  if (SEARCH || (!RAW && !JSON_OUTPUT)) {
     if (imports.length) {
-      if (ZERO) {
-        imports.forEach(im => console.log(im.importFile))
+      if (!SEARCH) {
+        if (ZERO) {
+          imports.forEach(im => console.log(im.importFile))
+        } else {
+          console.log()
+          console.log(colors.bold(`${filename}:`))
+          console.log(imports.map(im => {
+            return im.isAbsolute ? `- ${im.lineNo}: ${im.fullImportFile}` : `- ${im.lineNo}: ${im.importFile} -> ${im.fullImportFile}`
+          }).join('\n'))
+        }
       } else {
-        console.log()
-        console.log(colors.bold(`${filename}:`))
-        console.log(imports.map(im => {
-          return im.isAbsolute ? `- ${im.lineNo}: ${im.fullImportFile}` : `- ${im.lineNo}: ${im.importFile} -> ${im.fullImportFile}`
-        }).join('\n'))
+        const found = []
+        imports.forEach(im => {
+          const idx = im.fullImportFile.indexOf(SEARCH)
+          if (idx >= 0) {
+            found.push(`- ${im.lineNo}: ${im.fullImportFile.slice(0, idx)}${colors.bgYellow(SEARCH)}${im.fullImportFile.slice(idx + SEARCH.length, im.fullImportFile.length)}`)
+          }
+        })
+        if (found.length) {
+          console.log()
+          console.log(colors.bold(`${filename}:`))
+          found.forEach(line => console.log(line))
+        }
       }
     }
   }
