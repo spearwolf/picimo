60f27224a24dc494dba5081cff1f6d7f91814ccf
Wolfger Schramm <wolfger@spearwolf.de>
2017-12-10 15:40:35 +0100

    disarm rename.sh

--- rename.sh
+++ rename.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "rename: $1"
-sed -i \
-  -e 's/blitpunk-canvas/pi-canvas/g' \
-  -e 's/blitpunk-entity/pi-entity/g' \
-  -e 's/blitpunk-sprite-group/pi-sprite-group/g' \
-  -e 's/blitpunk-texture-atlas/pi-texture-atlas/g' \
-  -e 's/blitpunk-picture/pi-picture/g' \
-  -e 's/blitpunk/picimo/g' \
-  -e 's/BLITPUNK/PICIMO/g' \
-  -e 's/Blitpunk/Picimo/g' \
-  "$1"
+# sed -i \
+  # -e 's/blitpunk-canvas/pi-canvas/g' \
+  # -e 's/blitpunk-entity/pi-entity/g' \
+  # -e 's/blitpunk-sprite-group/pi-sprite-group/g' \
+  # -e 's/blitpunk-texture-atlas/pi-texture-atlas/g' \
+  # -e 's/blitpunk-picture/pi-picture/g' \
+  # -e 's/blitpunk/picimo/g' \
+  # -e 's/BLITPUNK/PICIMO/g' \
+  # -e 's/Blitpunk/Picimo/g' \
+  # "$1"
