f2032eeb6439b7c4f22c41a1ae30fce7a6b18cd2
Wolfger Schramm <wolfger@spearwolf.de>
2018-01-03 15:06:32 +0100

    code refactorings

---
+++ testrunner/tests/utils/matchScreenshot.js
@@ -0,0 +1,14 @@
+import expectToMatchScreenshot from './expectToMatchScreenshot'
+
+const containerEl = document.querySelector('.picimo')
+const visualRegressionsEl = document.querySelector('.visualRegressions__container')
+
+export const createHtml = html => {
+  containerEl.innerHTML = html
+}
+
+export const querySelector = (selector) => containerEl.querySelector(selector)
+export const getCanvasEl = () => querySelector('pi-canvas')
+
+export const screenshotUrl = imgPath => `/tests/screenshots/${imgPath}`
+export const matchScreenshot = imgPath => expectToMatchScreenshot(screenshotUrl(imgPath), getCanvasEl(), visualRegressionsEl)
