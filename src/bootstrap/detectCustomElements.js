4ea20868b215f69ea50389e7df6c256ba939ec5e
Wolfger Schramm <wolfger@spearwolf.de>
2018-01-30 10:30:58 +0100

    remove bowser (browser detection) dependency

--- src/bootstrap/detectCustomElements.js
+++ src/bootstrap/detectCustomElements.js
@@ -1,15 +1,6 @@
-const bowser = require('bowser')
-
 // http://caniuse.com/#feat=custom-elementsv1
 
 export default () => {
-  if (typeof customElements === 'undefined') return false
-  if (bowser.firefox || bowser.safari || bowser.ios || bowser.msie || bowser.msedge) return false
-  if (bowser.ios) return false  // TODO what about ios safari 10.3 or 11?
-  if (bowser.check({
-    chrome: '58',
-    opera: '46',
-    vivaldi: '1.92'
-  })) return true
-  return false // return false if unsure about browser support for customElements
+  if (typeof window.customElements !== 'undefined') return true
+  return false
 }
