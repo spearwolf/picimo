98055b94867416866aacb93dd8f0f8b95148dc66
Wolfger Schramm <wolfger@spearwolf.de>
2019-08-15 17:24:44 +0200

    remove picimo/examples

--- packages/picimo/examples/src/utils/appendTo.js
+++
@@ -1,2 +0,0 @@
-/* eslint-env browser */
-export const appendTo = (select, postfix = '') => text => document.querySelector(select).innerHTML += `${text}${postfix}`.replace(/\n/g, '<br>');
--- packages/picimo/examples/src/utils/makeWireframe.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from 'three';
-
-export const makeWireframe = (geometry, color = 0xffffff) => new THREE.LineSegments(
-  new THREE.EdgesGeometry(geometry),
-  new THREE.LineBasicMaterial({ color }),
-);
