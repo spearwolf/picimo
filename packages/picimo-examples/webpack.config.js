87df05c6343fcd9de5e7058073f321add2af8826
Wolfger Schramm <wolfger@spearwolf.de>
2019-08-15 17:22:41 +0200

    add PACKAGE_VERSION define

--- packages/picimo-examples/webpack.config.js
+++ packages/picimo-examples/webpack.config.js
@@ -2,6 +2,9 @@
 // tslint:disable:object-literal-sort-keys
 const path = require("path");
 
+const webpack = require('webpack');
+const picimoPackageJson = require('../picimo/package.json');
+
 const polyfills = ['core-js/stable', 'regenerator-runtime/runtime'];
 
 const examplesJson = require('./examples.json');
@@ -37,6 +40,11 @@ module.exports = {
     useLocalIp: true,
     disableHostCheck: true,
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      PACKAGE_VERSION: JSON.stringify(`${picimoPackageJson.version}-dev`),
+    }),
+  ],
   module: {
     rules: [
       {
