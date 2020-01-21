d5b8e7bb4893d0a1671779e336465007088e3338
Wolfger Schramm <wolfger@spearwolf.de>
2020-01-21 07:48:25 +0100

    set displayName

--- packages/picimo-r3f/src/components/SpriteGroupMesh.jsx
+++ packages/picimo-r3f/src/components/SpriteGroupMesh.jsx
@@ -5,19 +5,18 @@ import {node} from 'prop-types';
 
 extend({PicimoSpriteGroupMesh});
 
-// eslint-disable-next-line react/display-name
 export const SpriteGroupMesh = forwardRef(({ children, ...props }, ref) => (
   <picimoSpriteGroupMesh ref={ref} {...props}>
     {children}
   </picimoSpriteGroupMesh>
 ));
 
+SpriteGroupMesh.displayName = 'SpriteGroupMesh';
+
 SpriteGroupMesh.propTypes = {
   children: node,
-  // ref
 }
 
 SpriteGroupMesh.defaultProps = {
   children: undefined,
-  // ref
 }
