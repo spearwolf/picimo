f4db8df62d74deef586296e5ab1dda3456fd5588
Wolfger Schramm <wolfger@spearwolf.de>
2020-02-01 13:42:55 +0100

    improve <SpriteGroup> story

--- packages/picimo-r3f/stories/3-SpriteGroup.stories.js
+++ packages/picimo-r3f/stories/3-SpriteGroup.stories.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {Canvas} from 'react-three-fiber';
 import {Stage2D, SpriteGroupMesh, SimpleSpritesBufferGeometry, SimpleSpritesMaterial, TextureAtlas, Sprites} from 'picimo-r3f';
 import {withKnobs, boolean, select} from '@storybook/addon-knobs';
+import { Logger } from 'picimo';
 
 export default {
   title: 'SpriteGroup',
@@ -12,17 +13,37 @@ export default {
 
 const PROJECTION = {
   width: 640,
-  height: 400,
+  height: 480,
   fit: 'contain',
   distance: 1000,
   far: 10000,
 };
 
+const MIN_SIZE = 35;
+const MAX_SIZE = 150;
+
+const TEXTURE_ATLAS_SRC = [
+  'nobinger.json',
+  'amigaballs.json',
+  'clouds.json',
+  'starscape.json',
+];
+
+const SPRITES = 32;
+
 // TODO useProjection() hook?
+
+const log = new Logger('story');
 
 const createSprites = ({spriteGroup, textureAtlas}) => {
 
-  const sprites = spriteGroup.createSpritesFromTextures(textureAtlas.randomFrames(24));
+  const sprites = spriteGroup.createSpritesFromTextures(textureAtlas.randomFrames(SPRITES));
+
+  const halfOfSprites = Math.floor(SPRITES / 2);
+  const w = sprites.map(s => s.width).sort().slice(0, halfOfSprites).reduce((sum, w) => sum + w, 0) / halfOfSprites;
+  const scale = w > MIN_SIZE ? (w < MAX_SIZE ? 0 : MAX_SIZE / w) : MIN_SIZE / w;
+
+  log.log('average width of sprites:', w, textureAtlas);
 
   sprites.forEach(sprite => {
     const [x, y] = [
@@ -30,18 +51,23 @@ const createSprites = ({spriteGroup, textureAtlas}) => {
       (Math.random() * PROJECTION.height) - (PROJECTION.height * 0.5),
     ];
     sprite.translate(x, y, 0);
+
+    if (scale) {
+      sprite.width *= scale;
+      sprite.height *= scale;
+    }
   });
 
   return sprites;
 }
 
 const onCreate = ctx => {
-  console.log('[story] CREATE SPRITES', ctx);
+  log.log('CREATE SPRITES', ctx);
   return createSprites(ctx);
 }
 
 const onTextureAtlasChange = (ctx, sprites) => {
-  console.log('[story] TEXTURE-ATLAS CHANGE', ctx, sprites);
+  log.log('TEXTURE-ATLAS CHANGE', ctx, sprites);
   ctx.spriteGroup.voPool.free(sprites);
   return createSprites(ctx);
 }
@@ -50,13 +76,13 @@ export const SimpleSprites = () => {
   return (
     <section>
       <div style={{backgroundColor: '#d0e9f0'}}>
-        <Canvas gl2 pixelRatio={window.devicePixelRatio} style={{minHeight: '400px'}}>
+        <Canvas gl2 pixelRatio={window.devicePixelRatio} style={{minHeight: '480px'}}>
           <Stage2D plane="xz" type="parallax" projection={PROJECTION}>
 
             { boolean('render <TextureAtlas>', true) && (
               <TextureAtlas
                 name="spritesAtlas"
-                src={select('texture-atlas', ['nobinger.json', 'amigaballs.json'], 'nobinger.json')}
+                src={select('texture-atlas', TEXTURE_ATLAS_SRC, TEXTURE_ATLAS_SRC[0])}
               />
             )}
 
