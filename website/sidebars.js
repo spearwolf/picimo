9a069fc92b9f0d180035ebd1b61cda74e9e93d17
Wolfger Schramm <wolfger@spearwolf.de>
2020-05-08 10:43:41 +0200

    picimo: add first api-extractor/documenter integration (wip)

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,24 @@
+/* eslint-env node */
+const fs = require('fs');
+const path = require('path');
+
+const API = [];
+
+fs.readdirSync(path.join(__dirname, 'docs/api'), {encoding: 'utf-8'})
+  .sort()
+  .forEach((file) => {
+    apiFile = path.basename(file).replace(/\.md$/, '');
+    if (apiFile.match(/^[^.]+(\.[^.]+)?$/)) {
+      API.push(`api/${apiFile}`);
+    }
+  });
+
+console.dir(API);
+
 module.exports = {
   someSidebar: {
     'Getting Started': ['Introduction'],
     Guides: ['Sprites', 'Map2D', 'Projection'],
+    API,
   },
 };
