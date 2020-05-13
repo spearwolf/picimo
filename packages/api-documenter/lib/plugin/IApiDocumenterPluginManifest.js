64fb6f6a10e038610d099ebe2b6106d35ed91088
Wolfger Schramm <wolfger@spearwolf.de>
2020-05-13 11:25:03 +0200

    api-documenter: include lib/ output to git repos

---
+++ packages/api-documenter/lib/plugin/IApiDocumenterPluginManifest.js
@@ -0,0 +1,4 @@
+"use strict";
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+Object.defineProperty(exports, "__esModule", { value: true });
