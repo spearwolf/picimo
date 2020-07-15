fa200f40d01ffd049ad66330b3a24066e419c988
Wolfger Schramm <wolfger@spearwolf.de>
2020-07-15 09:25:24 +0200

    picimo: add Plane.threePlane property

--- packages/picimo/src/utils/math/Plane.ts
+++ packages/picimo/src/utils/math/Plane.ts
@@ -1,10 +1,18 @@
+import {Plane as ThreePlane, Vector3} from 'three';
+
 export class Plane {
   static XY = new Plane('xy');
   static XZ = new Plane('xz');
 
-  type: 'xy' | 'xz';
+  readonly type: 'xy' | 'xz';
+  readonly threePlane: ThreePlane;
 
   constructor(type: 'xy' | 'xz') {
     this.type = type;
+    if (type === 'xy') {
+      this.threePlane = new ThreePlane();
+    } else {
+      this.threePlane = new ThreePlane(new Vector3(0, 1, 0));
+    }
   }
 }
