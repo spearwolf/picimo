351c07146ddd251954d8058a86616275892ddc02
Wolfger Schramm <wolfger@spearwolf.de>
2020-12-07 08:17:00 +0100

    improve inline documentation

--- packages/picimo/src/projection/Stage2D.ts
+++ packages/picimo/src/projection/Stage2D.ts
@@ -7,48 +7,73 @@ import {IProjection} from '.';
 
 const log = new Logger('picimo.Stage2D');
 
+/**
+ * A `Stage2D` is a `THREE.Scene` with a `Projection`.
+ *
+ * This helper class does not bring new features, but simplifies the handling of
+ * a `THREE.Scene`, `THREE.Camera`, `Projection` and `Display`.
+ *
+ * It offers the `resize()` and `frame()` methods, with which you can easily connect to
+ * the 'Display' class: `display.on(stage2d)`
+ *
+ * The `resize()` method updates the projection and should be called whenever
+ * the 2d dimension of the render target changes.
+ *
+ * The `frame()` method renders the scene with the camera from the projection.
+ */
 export class Stage2D extends Scene {
   renderOnFrame = true;
 
-  private _projection: IProjection;
-  private _currentWidth: number = 0;
-  private _currentHeight: number = 0;
+  #projection: IProjection;
+
+  #curProjWidth = 0;
+  #curProjHeight = 0;
 
   constructor(projection?: IProjection) {
     super();
-    this._projection = projection;
+    this.#projection = projection;
   }
 
   set projection(projection: IProjection) {
-    this._projection = projection;
-    projection.update(this._currentWidth, this._currentHeight);
+    this.#projection = projection;
+    projection.update(this.#curProjWidth, this.#curProjHeight);
   }
 
   get projection(): IProjection {
-    return this._projection;
+    return this.#projection;
   }
 
   get camera(): Camera {
-    return this._projection?.camera;
+    return this.#projection?.camera;
   }
 
-  resize({width, height}: {width: number; height: number}) {
+  /**
+   * Update the projection.
+   * Should be called whenever the 2d dimension of the render target changes.
+   */
+  resize({width, height}: {width: number; height: number}): void {
     const {projection} = this;
     if (projection) {
-      this._currentWidth = width;
-      this._currentHeight = height;
-      projection.update(width, height);
-      if (log.DEBUG) {
-        log.log(
-          `resize: ${width}x${height}, projection: ${projection.width}x${projection.height}`,
-        );
+      const prevWidth = this.#curProjWidth;
+      const prevHeight = this.#curProjHeight;
+      if (prevWidth !== width || prevHeight !== height) {
+        this.#curProjWidth = width;
+        this.#curProjHeight = height;
+        projection.update(width, height);
+        if (log.DEBUG) {
+          log.log(
+            `resize: ${width}x${height}, projection: ${projection.width}x${projection.height}`,
+          );
+        }
       }
-    } else if (log.DEBUG) {
-      log.log(`resize: w=${width} h=${height}`);
     }
   }
 
-  frame({display}: {display: Display}) {
+  /**
+   * Render the scene with the camera from the projection.
+   * But does not do anything, if `renderOnFrame` is `false`.
+   */
+  frame({display}: {display: Display}): void {
     const {camera, renderOnFrame} = this;
     if (camera && renderOnFrame) {
       display.renderer.render(this, camera);
