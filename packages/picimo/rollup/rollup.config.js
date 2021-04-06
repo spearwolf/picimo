b78b9562e5dfbe0d4af8cfc12d3899ef4723bd74
Wolfger Schramm <wolfger@spearwolf.de>
2021-04-06 14:05:25 +0200

    refactor rollup config

---
+++ packages/picimo/rollup/rollup.config.js
@@ -0,0 +1,70 @@
+/* eslint-env node */
+import path from 'path';
+
+import typescript from '@rollup/plugin-typescript';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import {sizeSnapshot} from 'rollup-plugin-size-snapshot';
+import {terser} from 'rollup-plugin-terser';
+
+import createBannerPlugin from './bannerPlugin';
+import {externalsPlugin} from './externalsPlugin';
+import {makeVersionWithBuild} from './makeVersionWithBuild';
+
+const projectDir = path.resolve(path.join(path.dirname(__filename), '..'));
+const outputDir = projectDir;
+
+const packageJson = require(path.join(projectDir, 'package.json'));
+const {name} = packageJson;
+const version = makeVersionWithBuild('es2017')(packageJson.version);
+
+const extensions = ['.js', '.ts', '.json'];
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    name,
+    file: path.join(outputDir, `${name}.js`),
+    sourcemap: true,
+    sourcemapFile: path.join(outputDir, `${name}.js.map`),
+    format: 'esm',
+  },
+  plugins: [
+    externalsPlugin(),
+    typescript(),
+    createBannerPlugin({...packageJson, version}),
+    commonjs(),
+    resolve({
+      extensions,
+      customResolveOptions: {
+        moduleDirectory: 'node_modules',
+      },
+    }),
+    babel({
+      extensions,
+      rootMode: 'upward',
+      runtimeHelpers: true,
+      exclude: [/\/core-js\//, 'node_modules/@babel/**'],
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            corejs: {version: 3, proposals: true},
+          },
+        ],
+      ],
+    }),
+    replace({
+      NODE_ENV: JSON.stringify('production'),
+      PACKAGE_VERSION: JSON.stringify(version),
+      'log.VERBOSE': 'false',
+      'log.DEBUG': 'false',
+    }),
+    sizeSnapshot(),
+    terser({
+      output: {comments: /^!/},
+    }),
+  ],
+};
